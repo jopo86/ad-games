5561749247c137abfb5d0a4b280cc55c88fff264
Josh <joshrposner@icloud.com>
2024-05-25 21:51:39 -0400

    updated cannon ball logic, they can now rotate by random amnts

--- AdGames/src/Cannon.cpp
+++ AdGames/src/Cannon.cpp
@@ -15,7 +15,7 @@ const int SCR_WIDTH = 1280, SCR_HEIGHT = 720;
 const int FLOOR_HEIGHT = 150;
 const int CANNON_BODY_WIDTH = 100, CANNON_BODY_HEIGHT = 50;
 const int CANNON_BARREL_WIDTH = 30, CANNON_BARREL_HEIGHT = 100;
-const int BALL_RADIUS = 5;
+const int BALL_RADIUS = 10, BALL_SEGMENTS = 5;
 
 void Cannon::Run()
 {
@@ -49,8 +49,8 @@ void Cannon::Run()
     renderer.add(cannonBody);
 
     float vertices[12] = {
-        -CANNON_BARREL_WIDTH / 2, -CANNON_BARREL_HEIGHT / 4.0f, 0.0f,
-         CANNON_BARREL_WIDTH / 2, -CANNON_BARREL_HEIGHT / 4.0f, 0.0f,
+        -CANNON_BARREL_WIDTH / 2,    -CANNON_BARREL_HEIGHT / 4.0f, 0.0f,
+         CANNON_BARREL_WIDTH / 2,    -CANNON_BARREL_HEIGHT / 4.0f, 0.0f,
          CANNON_BARREL_WIDTH / 2, 3 * CANNON_BARREL_HEIGHT / 4.0f, 0.0f,
         -CANNON_BARREL_WIDTH / 2, 3 * CANNON_BARREL_HEIGHT / 4.0f, 0.0f
     };
@@ -65,9 +65,9 @@ void Cannon::Run()
     cannonBarrel.translate(Vec3(SCR_WIDTH / 2, FLOOR_HEIGHT + CANNON_BODY_HEIGHT, 0.0f));
     renderer.add(cannonBarrel);
 
-    Renderable cannonBall = Renderable::ColoredCircle(BALL_RADIUS, BALL_RADIUS, Vec4::Black());
+    Renderable cannonBall = Renderable::ColoredCircle(BALL_RADIUS, BALL_SEGMENTS, Vec4::Black());
 
-    std::list<std::pair<Vec2, Vec2>> cannonBalls;
+    std::list<Quadruple<Vec2, Vec2, float, float>> cannonBalls;
 
     input.setMouseButtonCooldown(MouseButton::Left, 0.1f);
 
@@ -75,7 +75,7 @@ void Cannon::Run()
     {
         double dt = window.getDeltaTime();
 
-        double deg = Degrees(atan2(input.getMousePos().getY() - cannonBarrel.getPosition().getY(), input.getMousePos().getX() - cannonBarrel.getPosition().getX()));
+        double deg = Degrees(Atan2(input.getMousePos().getY() - cannonBarrel.getPosition().getY(), input.getMousePos().getX() - cannonBarrel.getPosition().getX()));
         if (deg < 0) deg += 360;
         deg -= 90;
         if (deg > 180 && deg < 270) deg = -90;
@@ -86,9 +86,9 @@ void Cannon::Run()
         if (input.isMouseButtonPressed(MouseButton::Left))
         {
             Vec2 pos(cannonBarrel.getPosition().getX(), cannonBarrel.getPosition().getY());
-			Vec2 dir = Vec2(cos(Radians(Clamp(deg, -60, 60) + 90)), sin(Radians(Clamp(deg, -60, 60) + 90))).getNormalized();
-            pos += dir *  (3.0f * CANNON_BARREL_HEIGHT / 4.0f);
-			cannonBalls.push_back({ pos, dir * 500.0f });
+			Vec2 dir = Vec2(Cos(Radians(Clamp(deg, -60, 60) + 90)), Sin(Radians(Clamp(deg, -60, 60) + 90))).getNormalized();
+            pos += dir *  (3.0f * CANNON_BARREL_HEIGHT / 4.0f - BALL_RADIUS);
+			cannonBalls.push_back({ pos, dir * 500.0f, Math::Rand<float>(0.0f, 360.0f), Math::Rand<float>(50.0f, 360.0f) });
         }
 
         cam.update();
@@ -98,7 +98,7 @@ void Cannon::Run()
         window.startRender();
         renderer.render(); 
         
-        std::list<std::pair<Vec2, Vec2>>::iterator it = cannonBalls.begin();
+        std::list<Quadruple<Vec2, Vec2, float, float>>::iterator it = cannonBalls.begin();
         while (it != cannonBalls.end())
         {
             auto& ball = *it;
@@ -108,7 +108,9 @@ void Cannon::Run()
             }
 
             ball.first += ball.second * dt;
-            cannonBall.setPosition(Vec3(ball.first.getX(), ball.first.getY(), 0.0f));
+            ball.third += ball.fourth * dt;
+            cannonBall.setPosition(Vec3(ball.first.getX(), ball.first.getY(), -1.0f));
+            cannonBall.setRotation(Vec3(0.0f, 0.0f, ball.third));
             cannonBall.render(cam.getViewMatrix(), cam.getProjectionMatrix(), cam.getPosition());
 
             it++;
